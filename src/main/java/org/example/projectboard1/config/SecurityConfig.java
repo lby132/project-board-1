718e939b85e24d1a58b5ae6be9264a6abd81e75b
lby132 <quddbscjswow@naver.com>
2024-08-16 19:59:45 +0900

    jpa auditing, thymeleaf 설정

--- src/main/java/org/example/projectboard1/config/SecurityConfig.java
+++ src/main/java/org/example/projectboard1/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package org.example.projectboard1.config;
 
+import org.example.projectboard1.dto.security.BoardPrincipal;
+import org.example.projectboard1.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -29,6 +35,19 @@ public class SecurityConfig {
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+        return username -> userAccountService
+                .searchUser(username)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
