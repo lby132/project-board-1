138e752159e685912b1e8313a828eed38d6e280f
lby132 <quddbscjswow@naver.com>
2024-08-24 00:14:36 +0900

    해시테그 테스트 코드 작성, 게시글 댓글 서비스 리포지토리 생성

---
+++ src/main/java/org/example/projectboard1/service/ArticleCommentService.java
@@ -0,0 +1,69 @@
+package org.example.projectboard1.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.projectboard1.domain.Article;
+import org.example.projectboard1.domain.ArticleComment;
+import org.example.projectboard1.domain.UserAccount;
+import org.example.projectboard1.dto.ArticleCommentDto;
+import org.example.projectboard1.respository.ArticleCommentRepository;
+import org.example.projectboard1.respository.ArticleRepository;
+import org.example.projectboard1.respository.UserAccountRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComments(Long articleId) {
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.articleId());
+            UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+//            articleCommentRepository.save(dto.toEntity(article, userAccount));
+            ArticleComment articleComment = dto.toEntity(article, userAccount);
+
+            if (dto.parentCommentId() != null) {
+                ArticleComment parentComment = articleCommentRepository.getReferenceById(dto.parentCommentId());
+                parentComment.addChildComment(articleComment);
+            } else {
+                articleCommentRepository.save(articleComment);
+            }
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
+        }
+    }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) {
+                articleComment.setContent(dto.content());
+            }
+        } catch (Exception e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
+    }
+
+    public void deleteArticleComment(Long articleCommentId, String userId) {
+        articleCommentRepository.deleteByIdAndUserAccount_UserId(articleCommentId, userId);
+    }
+
+}
---
+++ src/test/java/org/example/projectboard1/service/ArticleCommentServiceTest.java
@@ -0,0 +1,12 @@
+package org.example.projectboard1.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("비즈니스 로직 - 댓글")
+@ExtendWith(MockitoExtension.class)
+public class ArticleCommentServiceTest {
+
+}
